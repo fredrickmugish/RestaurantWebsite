2acd0006f5ac3f0b329e3421b266fb1232bb69cc
fredrickmugish <fredrickgotfried@gmail.com>
2022-09-06 03:28:27 -0400

    tenth commit

--- resources/views/admin/menus/index.blade.php
+++ resources/views/admin/menus/index.blade.php
@@ -48,7 +48,7 @@
                             </td>
 
                             <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{$menu->description}}
+                                {{$menu->price}}
                             </td>
 
 
